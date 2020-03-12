8c6c0c829932e9897a52523e850607a6bbc65013
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2020-03-12 10:35:50 +0000

    Uninstall virtual env

--- pan-os-config.tf
+++ pan-os-config.tf
@@ -54,6 +54,7 @@ resource "null_resource" "panos_settings" {
     command = <<EOF
                 PATH=${path.module}/venv/bin:/usr/local/bin:$HOME/.local/bin:$PATH
                 export PYTHONHTTPSVERIFY=0
+                pip uninstall --user virtualenv
                 pip install --user virtualenv
                 if [ ! -d "${path.module}/venv" ]; then
                     pip install --user virtualenv
